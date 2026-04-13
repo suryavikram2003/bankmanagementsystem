47e55ceaaeba9809e7001b94f480cc4a9477dcb5
suryavikram2003 <69914435+suryavikram2003@users.noreply.github.com>
2026-04-13 21:53:46 +0530

    Update BankManagementSystemApplication.java

--- src/main/java/bank/management/system/BankManagementSystemApplication.java
+++ src/main/java/bank/management/system/BankManagementSystemApplication.java
@@ -2,15 +2,18 @@ package bank.management.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * Main Spring Boot Application class for Bank Management System
- * This replaces the individual main() methods in the Swing application
- */
 @SpringBootApplication
-public class BankManagementSystemApplication {
+public class BankManagementSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BankManagementSystemApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BankManagementSystemApplication.class);
     }
 }
